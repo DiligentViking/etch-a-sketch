271a7af54123bc1d63d58be9a5e0a86ab6ae9507
DiligentViking <acore-n@proton.me>
2025-03-21 16:25:18 -0500

    Revert back to 844a988

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-let size = 15;
-
-function makeGrid(gridSize) {
-  gridSize = gridSize * gridSize;
+function makeGrid(rows, columns) {
   const container = document.querySelector('#container');
-  for (i = 0; i < gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+  for (i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (j = 0; j < columns; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener('mouseover', () => {
+        square.classList.add('hover');
+      });
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
 }
 
 
-makeGrid(size);
-
-
-
-
-// function makeGrid(rows, columns) {
-//   const container = document.querySelector('#container');
-//   for (i = 0; i < rows; i++) {
-//     const row = document.createElement('div');
-//     row.classList.add('row');
-//     for (j = 0; j < columns; j++) {
-//       const square = document.createElement('div');
-//       square.classList.add('square');
-//       square.addEventListener('mouseover', () => {
-//         square.classList.add('hover');
-//       });
-//       row.appendChild(square);
-//     }
-//     container.appendChild(row);
-//   }
-// }
-// makeGrid(8, 8);
-
+makeGrid(8, 8);
