179ef16aa9aa66ff61b3fc25602e139b2372b63a
DiligentViking <acore-n@proton.me>
2025-03-21 17:32:48 -0500

    Add entire button functionality

--- script.js
+++ script.js
@@ -15,5 +15,30 @@ function makeGrid(rows, columns) {
   }
 }
 
-
 makeGrid(10, 10);
+
+
+function wipeGrid() {
+  document.querySelector('#container').remove();
+  const newContainer = document.createElement('div');
+  newContainer.setAttribute('id', 'container');
+  document.querySelector('#super-container').appendChild(newContainer);
+}
+
+const rows = document.querySelector('#rows');
+let numRows = rows.textContent.slice(6);
+const cols = document.querySelector('#cols');
+let numCols = rows.textContent.slice(6);
+
+rows.addEventListener('click', () => {
+  numRows = prompt('Enter number of rows', numRows);
+  wipeGrid();
+  makeGrid(numRows, numCols);
+  rows.textContent = rows.textContent.slice(0, 6) + numRows;
+})
+cols.addEventListener('click', () => {
+  numCols = prompt('Enter number of columns', numCols)
+  wipeGrid();
+  makeGrid(numRows, numCols);
+  cols.textContent = cols.textContent.slice(0, 6) + numCols;
+})
