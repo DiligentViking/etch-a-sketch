1c6e6c8fdc7af8d538568a99f3d57c5599bf4266
DiligentViking <acore-n@proton.me>
2025-03-21 14:05:55 -0500

    Add initial square generation (nonfunctional tho)

---
+++ script.js
@@ -0,0 +1,14 @@
+let size = 16;
+
+function makeGrid(gridSize) {
+  const container = document.querySelector('#container');
+  for (i = 0; i < gridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+}
+
+
+
+makeGrid(size);
